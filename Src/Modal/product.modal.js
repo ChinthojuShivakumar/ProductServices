0efb337a902bb204614172ec9321e7c38709ac7b
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2025-08-30 14:18:30 +0530

    time stamps

--- Src/Modal/product.modal.js
+++ Src/Modal/product.modal.js
@@ -1,75 +1,80 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  images: [
-    {
-      publicId: {
-        type: String,
-        required: true,
-      },
-      url: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
-
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    default: 0,
-  },
-  stock: {
-    type: Number,
-    required: true,
-  },
-  totalReviews: {
-    type: Number,
-    default: 0,
-  },
-  reviewList: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "reviews",
-  },
-  deleted: {
-    type: Boolean,
-    default: false,
-  },
-  deletedAt: {
-    type: Date,
-    default: null,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "category",
-  },
-  highlights: [
-    {
+const productSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
+      required: true,
     },
-  ],
-  specifications: {
-    type: Map,
-    of: String,
+    images: [
+      {
+        publicId: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    totalReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviewList: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "reviews",
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "category",
+    },
+    highlights: [
+      {
+        type: String,
+      },
+    ],
+    specifications: {
+      type: Map,
+      of: String,
+    },
+    discount: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
   },
-  discount: {
-    type: Number,
-    require: true,
-    default: 0,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 productSchema.index(
   { deletedAt: 1 },
   { expireAfterSeconds: 30 * 24 * 60 * 60 }
